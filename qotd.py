c988cf1573249588632ef3c9f9d9a7d85122ec36
Graham Stevens <graham@grh.am>
2016-01-18 12:47:20 +0000

    Initial Python skeleton file

---
+++ qotd.py
@@ -0,0 +1,9 @@
+"""Script to impliment QotD (RFC865) in Python3."""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
