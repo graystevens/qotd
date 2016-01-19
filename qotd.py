ab4c75efe0fb40d54c8b960594caa77c9722688c
Graham Stevens <graham@grh.am>
2016-01-19 19:43:21 +0000

    Working socket on port 17, returning a quote!

--- qotd.py
+++ qotd.py
@@ -1,20 +1,31 @@
 """Script to impliment QotD (RFC865) in Python3."""
 import sys
 import socket
+import csv
+import random
+
+def getQuote():
+	with open('quotes_all.csv', 'r') as allQuotes:
+		quotes = list(csv.reader(allQuotes, delimiter=';'))
+		randomQuote = random.choice(quotes)
+		quote = randomQuote[0] + "\n – " + randomQuote[1]
+		quote = quote.encode()
+		print (quote)
+		return quote
 
 def main():
     """Main entry point for the script."""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object
     host = socket.gethostname()	# Get local machine name
-    port = 17					# Reserve a port for your service.
+    port = 17				# Reserve a port for your service.
     s.bind((host, port))		# Bind to the port
 
     s.listen(5)					# Now wait for client connection.
     while True:
-        c, addr = s.accept()	# Establish connection with client.
-        print('Got connection from', addr)
-        c.send(b'Thank you for connecting')
-        c.close()				# Close the connection
+        (clientsocket, address) = s.accept()		# Establish connection with client.
+        print('Got connection from', address)
+        clientsocket.send(getQuote())				# Get a random quote!
+        clientsocket.close()						# Close the connection
         pass
 
 if __name__ == '__main__':
