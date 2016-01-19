2a4c116e4fa23bfad84f10c769b3a99a89f69039
Graham Stevens <graham@grh.am>
2016-01-19 20:23:35 +0000

    Allow connections via TCP & UDP on port 17

--- qotd.py
+++ qotd.py
@@ -4,30 +4,57 @@ import socket
 import csv
 import random
 import textwrap
+import select
 
 def getQuote():
-	with open('quotes_all.csv', 'r') as allQuotes:
-		quotes = list(csv.reader(allQuotes, delimiter=';'))
-		randomQuote = random.choice(quotes)
-		quote = textwrap.fill(randomQuote[0], width=60)
-		quote = quote + "\n\t\t\t – " + randomQuote[1]
-		quote = quote.encode()
-		return quote
+    with open('quotes_all.csv', 'r') as allQuotes:
+        quotes = list(csv.reader(allQuotes, delimiter=';'))
+        randomQuote = random.choice(quotes)
+        quote = textwrap.fill(randomQuote[0], width=60)
+        quote = quote + "\n\t\t\t – " + randomQuote[1]
+        quote = quote.encode()
+        return quote
+
+def tcpCon(connection):
+    (clientsocket, address) = connection.accept()# Establish connection with client.
+    print('Got TCP connection from', address)
+    clientsocket.send(getQuote())				# Get a random quote!
+    clientsocket.close()						# Close the connection
+
+def udpCon(connection):
+    (data, address) = connection.recvfrom(1)# Establish connection with client.
+    print('Got UDP connection from', address)
+    connection.sendto(getQuote(),address)               # Get a random quote!
 
 def main():
-    """Main entry point for the script."""
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object
+    #Main entry point for the script.
+    #Set some initial variables, applying to both sockets
+    port = 17
+    backlog = 5
     host = socket.gethostname()	# Get local machine name
-    port = 17				# Reserve a port for your service.
-    s.bind((host, port))		# Bind to the port
 
-    s.listen(5)					# Now wait for client connection.
+    #Setup TCP socket
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.bind((host, port))		# Bind to the port
+    tcp.listen(backlog)
+
+    #Setup UDP socket
+    udp =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp.bind((host, port))
+
+    choice = [tcp,udp]
+
     while True:
-        (clientsocket, address) = s.accept()		# Establish connection with client.
-        print('Got connection from', address)
-        clientsocket.send(getQuote())				# Get a random quote!
-        clientsocket.close()						# Close the connection
-        pass
+        inselect,outselect,exceptselect = select.select(choice,[],[])
+
+        for connection in inselect:
+            if connection == tcp:
+                tcpCon(connection)
+            elif connection == udp:
+                udpCon(connection)
+            else:
+                print("Error!")
+        
 
 if __name__ == '__main__':
     sys.exit(main())
