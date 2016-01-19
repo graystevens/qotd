ff257b6d1a06f24167056bc81a2033a992952d78
Graham Stevens <graham@grh.am>
2016-01-19 19:51:00 +0000

    Use port 17

--- qotd.py
+++ qotd.py
@@ -18,7 +18,7 @@ def main():
     """Main entry point for the script."""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object
     host = socket.gethostname()	# Get local machine name
-    port = 12345				# Reserve a port for your service.
+    port = 17				# Reserve a port for your service.
     s.bind((host, port))		# Bind to the port
 
     s.listen(5)					# Now wait for client connection.
