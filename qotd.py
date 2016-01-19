51ce8dffed2d9bd1354d9d7d188ed8d9fcdd31f2
Graham Stevens <graham@grh.am>
2016-01-19 19:50:34 +0000

    Tidy up received quote, using textwrap and some tabs

--- qotd.py
+++ qotd.py
@@ -3,21 +3,22 @@ import sys
 import socket
 import csv
 import random
+import textwrap
 
 def getQuote():
 	with open('quotes_all.csv', 'r') as allQuotes:
 		quotes = list(csv.reader(allQuotes, delimiter=';'))
 		randomQuote = random.choice(quotes)
-		quote = randomQuote[0] + "\n – " + randomQuote[1]
+		quote = textwrap.fill(randomQuote[0], width=60)
+		quote = quote + "\n\t\t\t – " + randomQuote[1]
 		quote = quote.encode()
-		print (quote)
 		return quote
 
 def main():
     """Main entry point for the script."""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object
     host = socket.gethostname()	# Get local machine name
-    port = 17				# Reserve a port for your service.
+    port = 12345				# Reserve a port for your service.
     s.bind((host, port))		# Bind to the port
 
     s.listen(5)					# Now wait for client connection.
